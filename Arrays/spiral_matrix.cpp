a157c217729f2a7bf958142cace01a9da3b5962a
pranav <pranavvyas2003@gmail.com>
2023-08-17 16:55:55 +0530

    update comment - spiral matrix

--- Arrays/spiral_matrix.cpp
+++ Arrays/spiral_matrix.cpp
@@ -3,6 +3,39 @@
 using namespace std;
 vector<int> spiralOrder(vector<vector<int>> &matrix)
 {
+
+    /*
+
+        RIGHT -> DOWN -> LEFT -> UP
+        
+        1    2    3    4    5
+        6    7    8    9   10
+        11   12   13   14  15
+        16   17   18   19  20
+        21   22   23   24  25 
+        26   27   28   29  31   
+
+        1    2    3    4    5
+        6                  10
+        11                 15
+        16                 20
+        21                 25  
+        26   27   28   29  31   
+
+
+       
+            7    8    9   
+           12         14  
+           17         19  
+           22         24   
+           27    28   29   
+
+
+                 13
+                 18
+                 23
+
+    */
     vector<int> ans;
     int topIdx = 0;
     int leftIdx = 0;
