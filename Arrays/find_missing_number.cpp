57f3a4b331574be2f991718ba037a524359f3dbd
pranav <pranavvyas2003@gmail.com>
2023-09-22 23:55:44 +0530

    update comments

--- Arrays/find_missing_number.cpp
+++ Arrays/find_missing_number.cpp
@@ -2,17 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int missingNumber(vector<int>& nums) {
-        int missingNumber(vector<int>& nums) {
-        int actualSum = 0;
+/*
+We are given the range and all the numbers are present except for one
+single number and that we have to find out
+for example - [3 0 1]
+the range would be => 0 - 3, but is missing here
+we can easily find out the missing number 
+step 1 - find out the total sum of this range - sum of n numbers ie 6 
+in this case
+step 2 - traverse the array and eliminate ie - substract arr[i] from total sum
+step 3 - at last the sum will get reduced to that missing number 
+step 4 - yes I came up with this approach by my self!!!
+*/
+  int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int nSum = (n * (n + 1))/2;
-        for(auto it : nums) {
-            actualSum+=it;
+        int sum = ( n * (n+1) ) / 2;
+        for(auto it: nums) {
+            sum = sum - it;
         }
-        return nSum - actualSum;
+        return sum;
     }
-}
 
 int main(){
 
