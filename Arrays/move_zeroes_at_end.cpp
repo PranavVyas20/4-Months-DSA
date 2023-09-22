74f78502d969cff63ec363be463cbb7384b00ed0
pranav <pranavvyas2003@gmail.com>
2023-09-22 23:40:18 +0530

    simplify two pointer approach and update comments

--- Arrays/move_zeroes_at_end.cpp
+++ Arrays/move_zeroes_at_end.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
 // Two pointer approach 
 // TC - 0(N)
+// The idea is to keep moving until we find a non zero element and then swap it with the pointer var
+// Example - 0 0 0 1 2 0 0 4
+// Initially ptr = 0 and we start a loop on the array
+// till idx 2, we dont do anything, but on idx 3 we find that the element is non-zero
+// so we swap nums[i] nums[ptr]
+// Now the array is - 1 0 0 0 2 0 0 4
+// we can keep doing this
+// Dry run this example - 1 2 3 0 5
 void moveZeroes(vector<int>& nums) {
-        int i;
-        for(int k = 0; k < nums.size(); k++) {
-            if(nums[k] == 0) {
-                i = k;
-                break;
-            }
-        }
-
-        for(int j = i+1; j < nums.size(); j++) {
-            if(nums[j] != 0) {
-                swap(nums[i], nums[j]);
-                i++;
+        int ptr = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[ptr]);
+                ptr++;
             }
         }
     }
