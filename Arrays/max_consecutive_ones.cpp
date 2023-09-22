c141cd1d68ed0195304754074dc11693237a4034
pranav <pranavvyas2003@gmail.com>
2023-09-23 00:40:51 +0530

    update comments with example

--- Arrays/max_consecutive_ones.cpp
+++ Arrays/max_consecutive_ones.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Good example - [1,1,0,1,1,1]
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int maxCount = INT_MIN;
         int count = 0;
@@ -12,19 +13,10 @@ using namespace std;
                 count = 0;
             }
         }
+        // why return max(count, maxcount) ?
+        // because after the first interruption ie non-one element the maxCount is 2
+        // now when the traversal will end the currentCount will be 3 but since the 
+        // else condition is not triggered, maxCount will not be updated to 3
         return max(count,maxCount);
-
-        // another approach
-        // int maxCount = INT_MIN;
-        // int count = 0;
-        // for(auto it: nums) {
-        //     if(it == 1) {
-        //         maxCount = max(count ++, maxCount);
-        //     }
-        //     else {
-        //         count = 0;
-        //     }
-        // }
-        // return count;
     }
 
