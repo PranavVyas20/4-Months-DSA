501a05ab00e6acaa633f15dfefb3cee94434cf7a
pranav <pranavvyas2003@gmail.com>
2023-09-25 18:39:27 +0530

    add comments

--- Arrays/sort_0_1_2_array.cpp
+++ Arrays/sort_0_1_2_array.cpp
@@ -35,9 +35,28 @@ void sortColors(vector<int> &nums)
         nums[i] = 2;
 }
 
-// Using Dutch national flag algorithm
-// TC - O(N)
-// why mid '<=' high - dry run this example - [2,0,1]
+/*
+Using Dutch national flag algorithm
+TC - O(N)
+why mid '<=' high - dry run this example - [2,0,1]
+
+We believe that our array is somewhat like this:
+    {0 0 0 0 0}       {1 1 1 1}         {2 1 0 1 2 1}        {2 2 2 2}
+   [0 --- low -1]   [low --- mid -1]   [mid --- high]   [high + 1 --- n - 1]    
+
+   1. 0 -> low - 1 => contains only 0s
+   2. low -> mid - 1 => contains only 1s
+   3. mid -> high => contains unsorted elements
+   4. high + 1 -> n - 1 => contains only 2s
+
+   We will use mid as an iterator and on iterating over each element we will put it in its correct
+   space 
+
+   Why mid <= high?
+   mid to high represents the space in which there are unsorted elements, but if mid get greater
+   than high - it means that that space is no longer there.
+
+*/
 vector<int> sortColorsDNF(vector<int>& nums) {
     int low = 0;
     int mid = 0;
