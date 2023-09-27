14367ba05750990d4af0a10225dbaa37f61cb965
pranav <pranavvyas2003@gmail.com>
2023-09-27 16:31:12 +0530

    better soln

--- Arrays/set_zeroes.cpp
+++ Arrays/set_zeroes.cpp
@@ -2,41 +2,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-    void setRow(vector<vector<int>>& grid, int row) {
-        // transform entire row into -1
+    void makeRowZero(int row, vector<vector<int>>& grid) {
         for(int i = 0; i < grid[0].size(); i++) {
             grid[row][i] = 0;
         }
     }
-    void setCol(vector<vector<int>>& grid, int col) {
-        // transform entire col into -1
+
+    void makeColZero(int col, vector<vector<int>>& grid) {
         for(int i = 0; i < grid.size(); i++) {
             grid[i][col] = 0;
         }
     }
-
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<bool> row(matrix.size(), false);
-        vector<bool> col(matrix[0].size(), false);
-
+        vector<pair<int, int>> dp;
         for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[i].size(); j++) {
+            for(int j = 0; j < matrix[0].size(); j++) {
                 if(matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+                    dp.push_back({i, j});
                 }
             }
         }
 
-        for(int i = 0; i < row.size(); i++) {
-            if(row[i] == true) {
-                setRow(matrix, i);
-            }
-        }
-
-        for(int i = 0; i < col.size(); i++) {
-            if(col[i] == true) {
-                setCol(matrix, i);
-            }
+        for(auto it: dp) {
+            makeRowZero(it.first, matrix);
+            makeColZero(it.second, matrix);
         }
     }
